6314ad816b28903d45a5f87d523e0b79c3d881a4
GangstaWangstaPranksta <49811885+GangstaWangstaPranksta@users.noreply.github.com>
2023-01-30 19:10:21 -0500

    initial commit

---
+++ api/index.ts
@@ -0,0 +1,37 @@
+import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
+import { ServerRequest } from "https://deno.land/std@0.58.0/http/server.ts";
+
+
+
+
+async function scrape(url) {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        //headless: false
+    });
+    let page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+    await page.setRequestInterception(true);
+        page.on('request', (req) => {
+            if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image' || req.resourceType() == 'script' || req.resourceType() == 'media'){
+                req.abort();
+            }
+            else {
+                req.continue();
+            }
+        });
+    await page.goto(url);
+
+    await page.waitForSelector('some-selector');
+    let text = await page.$eval('some-selector', el => el.value);
+    let json = JSON.parse(text);
+    
+    await page.close();
+    await browser.close();
+    return await json;
+}
+
+
+export default async (req: ServerRequest) => {
+    req.respond({ body: JSON.stringify(await scrape("some-url")) });
+  }
